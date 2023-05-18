965290d2b200661a4f4f239bf04e4e9a4eea01f6
kmaddox2122 <kmaddox2122@gmail.com>
2023-05-18 15:54:02 -0500

    Added homepage component and added button routing to recipe list page

--- angular-recipes/src/app/app.module.ts
+++ angular-recipes/src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipeListComponent
+    RecipeListComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
+    MatButtonModule,
     AppRoutingModule,
     RouterModule.forRoot([
-      {path: 'recipe-list', component: RecipeListComponent}
-    ])
+      {path: 'recipe-list', component: RecipeListComponent},
+      {path: 'home', component: HomepageComponent},
+      {path: '', component: HomepageComponent},
+      {path: '*', component: HomepageComponent},
+
+    ]),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
