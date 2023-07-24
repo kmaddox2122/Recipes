d035d9ea2bcfe8d3c6b2559551bc9f4aa72dfb78
kmaddox2122 <kmaddox2122@gmail.com>
2023-07-24 14:23:33 -0500

    added box shaddow to recipe list

--- digital-cookbook/src/app/homepage/homepage.component.ts
+++ digital-cookbook/src/app/homepage/homepage.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent {
 
+
+export class HomepageComponent implements OnInit{
+
+
+  //practice using an event listener to log to the console.
+  ngOnInit() {
+    const button = document.querySelector('button');
+    if (button) {
+      button.addEventListener('click', this.sayHello);
+    }
+  }
+  sayHello() {
+    console.log("Hello!");
+  }
+  
 }
